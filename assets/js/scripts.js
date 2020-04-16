8c404da0752ce7b1b83d9d8bf5751c5d9b62ee49
Francesco <francesco.scarnera3@gmail.com>
2020-04-16 18:18:16 +0200

    to fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,61 @@
-console.log('staiul');
+$(document).ready(function(){
+
+    // reference
+
+    var todoItems = [   'open Zoom', 
+                        'listen to the lesson',
+                        'coding',
+                        'run 10 km',
+                        'read a book'
+                    ];
+    
+    var list = $('.todo');
+    var newInput = $('.add-element');
+
+    // add element todo list
+
+    for (var i = 0; i < todoItems.length; i++) {
+
+        // call template
+        var elementList = $('.template li').clone();
+        // add dynamic text a template
+        elementList.prepend(todoItems[i]);
+        //add clone to list
+        list.append(elementList);
+    }
+
+    // remove todo items 
+
+    $('body').on('click', '.todo li i', function() {
+        $(this).parent().remove();
+    });
+
+    // add new todo items
+    
+    newInput.keyup( function(event) {
+
+        if (event.which == 13 || event.keyCode == 13) {
+            var text = newInput.val().trim();
+            console.log(event.which);
+            // validation
+            if (text !== '') {
+                var elementNew = $('.template li').clone();
+                elementNew.prepend(text);
+                list.append(elementNew);
+                // clear input
+                newInput.val('');
+            }
+        }
+
+        // todo item completed
+
+        $('body').on('click','.todo li', function(){
+            $(this).toggleClass('completed');
+        });
+
+    });
+
+
+
+
+}); // end jQuery
